5580347f68c978eb744d18b027c913bbab9d6532
Haktan Albayrak <78109456+HaktanAlbayrak@users.noreply.github.com>
2022-10-05 17:27:20 +0300

    profile cart added and redux infrastructure created

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 
 // components
 import Header from "./components/Header";
+import ProfileCard from "./components/ProfileCard";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProfileCard />
     </div>
   );
 }
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/profileCard.scss";
+import billGatesImage from "../assets/billgates.jpg";
+
+const ProfileCard = () => {
+  return (
+    <div className="card-wrapper">
+      <div className="card-inner">
+        <img src={billGatesImage} alt="bill-gates" className="card-image" />
+        <p className="card-text">Spend Bill Gates' Money</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
